b269d7eb5c1cbcb87f19d215cecea4e1e0dea4bb
Brandon Haun <haungo@gmail.com>
2025-08-02 00:26:21 -0400

    ..

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['sharp'],
-  basePath: '/bugipsum',
-  assetPrefix: '/bugipsum',
-  trailingSlash: true,
 };
 
 export default nextConfig;
